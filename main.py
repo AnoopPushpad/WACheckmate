e15c181b78fb6eac6c3ed0fa64f15296e261e6f5
Anoop Pushpad <70333442+AnoopPushpad@users.noreply.github.com>
2024-04-26 00:49:59 +0530

    Update main.py.

--- main.py
+++ main.py
@@ -5,24 +5,28 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from datetime import datetime
 
-#Constants
+# Constants
+WAIT_TIME = 30  # The time interval between checking different numbers should be adjusted based on your internet speed.
 
-WAIT_TIME = 30 #The time interval between checking different numbers should be adjusted based on your internet speed.
-
+# Function to check if a number is available on WhatsApp
 def is_number_available(driver, number, csv_writer):
     # Get the HTML source of the page
     checkingpagesrc = driver.page_source
 
     # Check if the specified HTML code snippet is present in the page source
-    if '<span dir="ltr" aria-label="" class="_ao3e" style="min-height: 0px;">Messages are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them. Click to learn more</span>' in checkingpagesrc:
+    if (
+        '<span dir="ltr" aria-label="" class="_ao3e" style="min-height: 0px;">Messages are end-to-end encrypted. No one outside of this chat, not even WhatsApp, can read or listen to them. Click to learn more</span>'
+        in checkingpagesrc
+    ):
         print(number + " is Available!")
         csv_writer.writerow([number, "Available"])
     else:
         print(number + " is not Available!")
         csv_writer.writerow([number, "Not Available"])
 
-introart = """
 
+# ASCII art for introduction
+introart = """
 ██     ██  █████   ██████ ██   ██ ███████  ██████ ██   ██ ███    ███  █████  ████████ ███████ 
 ██     ██ ██   ██ ██      ██   ██ ██      ██      ██  ██  ████  ████ ██   ██    ██    ██      
 ██  █  ██ ███████ ██      ███████ █████   ██      █████   ██ ████ ██ ███████    ██    █████   
@@ -32,44 +36,64 @@ introart = """
                                                                                               
 """
 
+# Function to generate CAPTCHA for user verification
 def generate_captcha():
     # Generate two random numbers
     num1, num2 = random.randint(1, 9), random.randint(1, 9)
-    
+
     # Compute the sum
     captcha_sum = num1 + num2
-    
-    
+
     while True:
         # Ask the user for input
-        print("Please solve the CAPTCHA only after logging into WhatsApp Web.")
-        user_input = int(input(f"Please enter the sum {num1} + {num2} to proceed: "))
-        
+        # time.sleep(10)
+        try:
+            user_input = int(
+                input(f"Please enter the sum {num1} + {num2} to proceed: ")
+            )
+        except ValueError:
+            print("Invalid input. Please enter a valid integer.")
+            continue
         # Check if the user's input is correct
         if user_input == captcha_sum:
-            print("CAPTCHA successfully solved! WhatsApp number verification process initiated.")
+            print(
+                "CAPTCHA successfully solved! WhatsApp number verification process initiated."
+            )
             break
         else:
             print("CAPTCHA failed. Please try again.")
 
 
+# Main program starts here
 print(introart)
 print("WACheckmate - WhatsApp Bulk Number Checker & Validator Tool\n")
-print("WACheckmate is a bulk WhatsApp number checking tool crafted with Python and utilizing Selenium.\n")
+print(
+    "WACheckmate is a bulk WhatsApp number checking tool crafted with Python and utilizing Selenium.\n"
+)
 print("1. Configure Parameters and Initiate Automation:")
-print("- Adjust the WAIT_TIME parameter to match your internet speed (default is 30 seconds).")
+print(
+    "- Adjust the WAIT_TIME parameter to match your internet speed (default is 30 seconds)."
+)
 print("- Press the enter button to start the automation process.")
 
 print("\n2. Log into WhatsApp Account and Solve Captcha:")
 print("- Scan the provided QR code to log into your WhatsApp account.")
-print("- After logging into WhatsApp, solve the basic captcha through the terminal when prompted.")
+print(
+    "- After logging into WhatsApp, solve the basic captcha through the terminal when prompted."
+)
 
 print("\n3. Validate Numbers with Country Code:")
-print("- Ensure that numbers in the numbers.txt file include the country code format, like +918516853332 (where +91 represents the country code).")
-print("- The script will then extract and validate each number listed in the numbers.txt file.")
+print(
+    "- Ensure that numbers in the numbers.txt file include the country code format, like +918516853332 (where +91 represents the country code)."
+)
+print(
+    "- The script will then extract and validate each number listed in the numbers.txt file."
+)
 
 print("\n4. Results:")
-print("- After validation, the results will be saved in a CSV file within the folder.\n")
+print(
+    "- After validation, the results will be saved in a CSV file within the folder.\n"
+)
 
 input("Please press Enter key to continue.")
 
@@ -80,8 +104,10 @@ base_url = "https://web.whatsapp.com"
 driver = webdriver.Chrome()
 driver.maximize_window()
 
-# Open WhatsApp Web
+# Open WhatsApp Web and generate CAPTCHA
 driver.get(base_url)
+time.sleep(10)  # Wait to login into WhatsApp!
+print("Please solve the CAPTCHA only after logging into WhatsApp Web.")
 generate_captcha()
 
 # Generate CSV file name with microseconds
@@ -105,4 +131,6 @@ with open("numbers.txt", "r") as file, open(csvfilename, "w", newline="") as csv
 
 # Close the WebDriver session
 driver.quit()
-print(f"WhatsApp number checking completed! The results have been saved into {csvfilename}.csv")
+print(
+    f"WhatsApp number checking completed! The results have been saved into {csvfilename}.csv"
+)
